061bc231d8f5cf02ba6f1a965189c90962ed00ea
Cai Lu <30845427+cailinglu@users.noreply.github.com>
2017-09-28 12:51:02 -0700

    (test) DB books and reviews are displayed on books index page

--- apps/books/tests.py
+++ apps/books/tests.py
@@ -2,25 +2,97 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.http import HttpRequest
 
+import copy
+
 from .models import Book
 from ..authors.models import Author
+from ..users.models import User
+from ..reviews.models import Review
+
+VALID_USER = {
+  "name": "Alice",
+  "alias": "alice123",
+  "email": "alice@123.com",
+  "password": "thisIsAGreatPassword",
+  "confirm": "thisIsAGreatPassword"
+}
 
 BOOK_JUSTICE = {
   "title": "Justice",
   "author": {
     "name": "Michael Sandel"
+  },
+  "review": {
+    "text": "The book and lectures are thought provoking",
+    "rating": 4
   }
 }
 
 class BookModel(TestCase):
   def test_save_and_retrieve_book(self):
-    justice_author = Author.objects.create(
-      name=BOOK_JUSTICE["author"]["name"]
-    )
+    book = add_book_to_db(BOOK_JUSTICE)
+    self.assertEqual(book.title, BOOK_JUSTICE["title"])
+    self.assertEqual(book.author.name, BOOK_JUSTICE["author"]["name"])
 
-    justice_book = Book.objects.create(
-      title=BOOK_JUSTICE["title"],
-      author=justice_author
-    )
-    self.assertEqual(justice_book.title, BOOK_JUSTICE["title"])
-    self.assertEqual(justice_book.author.name, BOOK_JUSTICE["author"]["name"])
+class BooksIndexPage(TestCase):
+  def test_redirects_to_home_if_user_is_not_logged_in(self):
+    # make sure no user is logged in
+    self.client.get(reverse("users:logout"))
+    response = self.client.get(reverse("books:index_page"))
+    self.assertEqual(response["location"], reverse("home"))
+  
+  def test_db_books_are_displayed(self):
+    book = add_book_to_db(BOOK_JUSTICE)
+    login(self)
+    response = self.client.get(reverse("books:index_page"))
+    html = response.content.decode("utf8")
+    self.assertIn(book.title, html)
+
+  def test_db_reviews_are_displayed(self):
+    # clear the reviews in the db
+    try:
+      Review.objects.all().delete()
+    except:
+      pass
+    
+    user = login(self)
+    book = add_book_to_db(BOOK_JUSTICE)
+
+    review_details = copy.copy(BOOK_JUSTICE)
+    review_details["review"]["user"] = user
+    review_details["review"]["book"] = book
+    review = add_review_to_db(review_details)
+
+    expected_string = str(user.alias) + " says: " + str(review.text)
+
+    response = self.client.get(reverse("books:index_page"))
+    html = response.content.decode("utf8")
+    self.assertIn(expected_string, html)
+
+def add_book_to_db(book):
+  author = Author.objects.create(
+    name=book["author"]["name"]
+  )
+  book = Book.objects.create(
+    title=book["title"],
+    author=author
+  )
+  return book
+
+def add_review_to_db(details):
+  review = Review.objects.create(
+    text=details["review"]["text"],
+    rating=details["review"]["rating"],
+    user=details["review"]["user"],
+    book=details["review"]["book"]
+  )
+  return review
+
+def login(self):
+  try:
+    User.objects.get(email=VALID_USER["email"]).delete()
+  except:
+    pass
+  user = User.objects.validate_registration(VALID_USER)
+  self.client.post(reverse("users:login"), VALID_USER)
+  return user
--- apps/books/views.py
+++ apps/books/views.py
@@ -4,6 +4,17 @@ from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
 from django.contrib import messages
+from django.views import View
+
+from .models import Book
+from ..reviews.models import Review
 
 def index_page(request):
-  return render(request, "books/index.html")
+  if request.session["user_email"]:
+    # get all books in the db
+    context = {
+      "books": Book.objects.all(),
+      "reviews": Review.objects.all()
+    }
+    return render(request, "books/index.html", context)
+  return redirect("home")
